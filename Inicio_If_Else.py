855ff76ef2f136d3e9bc3ee276f53c4198f1484b
Thiago Oliveira <112381623+thiagooliveirt@users.noreply.github.com>
2023-04-19 17:08:34 -0300

    Create Inicio_If_Else.py

---
+++ Inicio_If_Else.py
@@ -0,0 +1,7 @@
+entrada = input('Voce quer "entrar" ou "sair" ? ')
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Digito invalido')
