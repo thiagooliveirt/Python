8031aa091e0719f52699e41701f8c726388566bc
Thiago Oliveira <112381623+thiagooliveirt@users.noreply.github.com>
2023-04-19 18:40:17 -0300

    Update exc_do_chat_gpt.py

--- exc_do_chat_gpt.py
+++ exc_do_chat_gpt.py
@@ -1,4 +1,4 @@
-#Verifique se um número é positivo, negativo ou zero.
+
 #Verifique se um número é par ou ímpar.
 #Verifique se uma pessoa é maior ou menor de idade.
 #Calcule o maior entre dois números.
