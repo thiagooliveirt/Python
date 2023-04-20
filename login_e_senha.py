be59fbd883e454e04ab187de9344ae8328b9c10b
Thiago Oliveira <112381623+thiagooliveirt@users.noreply.github.com>
2023-04-20 07:28:59 -0300

    Update login_e_senha.py

--- login_e_senha.py
+++ login_e_senha.py
@@ -1,12 +1,13 @@
 entrada = input('[E]ntrar [S]air: ')
 
-if entrada == 'S':
+if entrada == 'S' or entrada == 's':
     print('Saindo...')
 else:
     senha_digitada = input('Senha: ')
     senha_permitida = '123456'
 
-    if entrada == 'E' and senha_digitada == senha_permitida:
+    if entrada == 'E' or entrada == 'e' and senha_digitada == senha_permitida:
         print('Login Efetuado!')
     else:
         print('Senha Incorreta')
+
